d7c5cc29bcd255b90a446eb198cdf0e2d1c1c3b8
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-25 16:41:59 +0100

    control flow logic

--- src/index.js
+++ src/index.js
@@ -1,79 +1,85 @@
-Remember the acronym UPS check
+// Remember the acronym UPS check
 
-Step One (Understand). We have to understand the problem.
-1. What is the unknown?
+// Step One (Understand). We have to understand the problem.
+// 1. What is the unknown?
 
-head of a new linkedList equal to the sum of the integers of the two Linked lists
-is each integer in the new Linked List a unique integer? no
+// head of a new linkedList equal to the sum of the integers of the two Linked lists
+// is each integer in the new Linked List a unique integer? no
 
-2. What are the data inputs?
+// 2. What are the data inputs?
 
-two non-negative linked lists of potentially unequal length 
-first node reprsents the least linked List
-is is sorted in ascending order? 
-do we need to filter out negative integers?
+// two non-negative linked lists of potentially unequal length
+// first node reprsents the least linked List
+// is is sorted in ascending order?
+// do we need to filter out negative integers?
 
-value of linked list is always in the range 0 - 9
+// value of linked list is always in the range 0 - 9
 
-cannot modify either of the data input linked lists 
-3. Can you restate the problem in your own words?
-traverse to null and make the linkedListOne.value the first digit of the 
-variable firstNumber and add to when you traverse to null in linkedListTwo.value 
-and make secondNumber
-add firstNumber + secondNumber
-
-4. Can the unknown be determined from the data inputs?
-yes
-
-
-5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory? 
-
-sufficient
-6. 
-How should I label important pieces of data input that are a part of the problem?
-
-linkedListOne, linkedListTwo
-
-7. Draw a figure. Introduce suitable notation.
-
-Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found. 
-8.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem? 
-9.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method? 
-10. Start with a simple example. Could you restate the problem? Could you restate it still differently? 
-11. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs? 
-12. Progress to more complex examples. What is your updated plan here to find a solution?
-13. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
-
-
-Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
-14.  Can you see clearly that the step is correct?
-
-
-IV. 	Step Four (Check). Examine the solution obtained and refactor. 
-15.  Can you check your result? 
-16.  Can you check the argument? 
-17.  Can you derive the result differently? 
-18.  Can you see it at a glance? 
-19.  Can you make the code DRYer and refactor? 
-20.  Can you improve the performance? 
-21.   How have other people solved this problem?
-
-
-function sumOfLinkedList (linkedListOne, linkedListTwo){
-// result = 0
-  let result = 0;
-// traverse to null and make the linkedListOne.value the first digit of the 
-// variable firstNumber and add to when you traverse to null in linkedListTwo.value 
+// cannot modify either of the data input linked lists
+// 3. Can you restate the problem in your own words?
+// traverse to null and make the linkedListOne.value the first digit of the
+// variable firstNumber and add to when you traverse to null in linkedListTwo.value
 // and make secondNumber
-  if ((typeOf linkedListOne = number && linkedListOne > 0 && linkedListOne <= 9){
-    while (linkedListOne)
+// add firstNumber + secondNumber
+
+// 4. Can the unknown be determined from the data inputs?
+// yes
+
+// 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory?
+
+// sufficient
+// 6.
+// How should I label important pieces of data input that are a part of the problem?
+
+// linkedListOne, linkedListTwo
+
+// 7. Draw a figure. Introduce suitable notation.
+
+// Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found.
+// 8.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem?
+// 9.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method?
+// 10. Start with a simple example. Could you restate the problem? Could you restate it still differently?
+// 11. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs?
+// 12. Progress to more complex examples. What is your updated plan here to find a solution?
+// 13. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
+
+// Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
+// 14.  Can you see clearly that the step is correct?
+
+// IV. 	Step Four (Check). Examine the solution obtained and refactor.
+// 15.  Can you check your result?
+// 16.  Can you check the argument?
+// 17.  Can you derive the result differently?
+// 18.  Can you see it at a glance?
+// 19.  Can you make the code DRYer and refactor?
+// 20.  Can you improve the performance?
+// 21.   How have other people solved this problem?
+
+function sumOfLinkedList(linkedListOne, linkedListTwo) {
+  // result = 0
+  let result = 0;
+  let newArr = [];
+  // traverse to null and make the linkedListOne.value the first digit of the
+  // variable firstNumber and add to when you traverse to null in linkedListTwo.value
+  // and make secondNumber
+  if (linkedListOne > 0 && linkedListOne <= 9) {
+    let currentOne = head;
+    while (currentOne !== null) {
+      newArr.push(linkedListOne.value);
+      currentOne = currentOne.next;
+      console.log(newArr);
+    }
   }
-  
-  if (typeOf linkedListTwo = number && linkedListTwo > 0 && linkedListTwo <= 9) {
 
-  } 
+  if (linkedListTwo > 0 && linkedListTwo <= 9) {
+    let currentTwo = head;
+    while (currentTwo !== null) {
+      newArr.push(linkedListTwo.value);
+      currentOne = currentOne.next;
+    }
+  }
 
-// convert it to a string and then add firstNumber + secondNumber
-// add it to result
-// split result into char and iterate through each char to add into an array
+  // convert it to a string and then add firstNumber + secondNumber
+  // add it to result
+  // split result into char and iterate through each char to add into an array
 }
